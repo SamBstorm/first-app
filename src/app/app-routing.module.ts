ff7579d42476d3e3ca3c73b1e4c2e79890e2e04c
Legrain Samuel <samuel.legrain@bstorm.be>
2021-08-03 12:04:47 +0200

    Add Bindings part1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
+import { BindingTwoWayComponent } from './components/binding-two-way/binding-two-way.component';
 import { HomeComponent } from './components/home/home.component';
 import { CoucouComponent } from './exemple/components/coucou/coucou.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BindingEventComponent } from './components/binding-event/binding-event.component';
 
 const routes: Routes = [
   {path:"home", component:HomeComponent , children:[
     {path:'coucou', component:CoucouComponent}
   ]},
+  {path:"two-way", component:BindingTwoWayComponent},
+  {path:"event", component:BindingEventComponent},
 ];
 
 @NgModule({
