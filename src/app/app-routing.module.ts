d71c7e2d42e07de02d5f835090e3ca44dcdc72a0
Legrain Samuel <samuel.legrain@bstorm.be>
2021-08-05 13:53:51 +0200

    InputOutput + Services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ParentComponent } from './components/parent/parent.component';
 import { DirectStructComponent } from './components/direct-struct/direct-struct.component';
 import { DirectCompComponent } from './components/direct-comp/direct-comp.component';
 import { PipesJsonComponent } from './components/pipes-json/pipes-json.component';
@@ -8,6 +9,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BindingEventComponent } from './components/binding-event/binding-event.component';
 import { BindingAttributeComponent } from './components/binding-attribute/binding-attribute.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 const routes: Routes = [
   {path:"home", component:HomeComponent , children:[
@@ -19,6 +21,8 @@ const routes: Routes = [
   {path:"pipe-json", component:PipesJsonComponent},
   {path:"direct-comp", component:DirectCompComponent},
   {path:"direct-struct", component:DirectStructComponent},
+  {path:'outputinput', component:ParentComponent},
+  {path:'shop', component:ShopComponent}
 ];
 
 @NgModule({
